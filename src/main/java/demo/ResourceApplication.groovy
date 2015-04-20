6f886623b57806960f16e7b01da98c93cd1d1bce
Javier Badaracco <javierbada@hotmail.com>
2015-04-20 02:42:23 -0300

    first commit

---
+++ src/main/java/demo/ResourceApplication.groovy
@@ -0,0 +1,34 @@
+package demo
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+class ResourceApplication {
+
+  @RequestMapping('/')
+  def home() {
+    [id: UUID.randomUUID().toString(), content: 'Hello World']
+  }
+
+  static void main(String[] args) {
+    SpringApplication.run ResourceApplication, args
+  }
+
+  @Bean
+  HeaderHttpSessionStrategy sessionStrategy() {
+    new HeaderHttpSessionStrategy();
+  }
+  
+}
